d99efcdda1dda8bed8b261df5cdd7ebbeb93d15e
ravaljhdp <103162756+ravaljhdp@users.noreply.github.com>
2022-05-06 11:07:54 +0530

    Create AUM 4.cpp

---
+++ AUM 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int x,y;
+cout<<"enter the value of x";
+cin>>x;
+cout<<"enter the value of y";
+cin>>y;
+cout<<"sutraction of"<<(x-y);
+getch();
+return 0;
+}
